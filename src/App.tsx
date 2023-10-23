730fa9c59bad11dca949339e6de1a50b113bc634
Duishenalieva <liltril783@gmail.com>
2023-10-23 17:15:13 +0600

    level 3: display the "deal cards" button in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import CardElement from "./Card/Card";
+import CardDeck from "./lib/CardDeck";
+import Card from './lib/Card';
 
 function App() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [cardDeck, setCardDeck] = useState(new CardDeck());
+
+  const dealCards = () => {
+    const cardsArray = cardDeck.getCards(5);
+    setCards(cardsArray);
+  };
   return(
     <>
+      <button className='deal-cards' onClick={dealCards}>Deal cards</button>
       <div className="playingCards faceImages">
-        <CardElement rank="K" suit="diams"/>
+        {cards.map((i, key) => {
+          return(<CardElement rank={i.rank} suit={i.suit} key={key}/>)
+          })}
       </div>
     </>
   );
