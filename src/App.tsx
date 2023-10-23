433a5010cc603fdadc0688fcc62358bca068d9d9
Duishenalieva <liltril783@gmail.com>
2023-10-23 16:52:56 +0600

    level 1: created a card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import CardElement from "./Card/Card";
+
 function App() {
   return(
-    <></>
+    <>
+      <div className="playingCards faceImages">
+        <CardElement rank="K" suit="diams"/>
+      </div>
+    </>
   );
 };
 
---
+++ src/Card/Card.tsx
@@ -0,0 +1,33 @@
+interface Props {
+    rank: string;
+    suit: string;
+};
+
+const CardElement = ({rank, suit}: Props) => {
+    let suitSymbol: string;
+
+    switch (suit){
+        case 'diams':
+            suitSymbol = '♦';
+            break;
+        case 'hearts':
+            suitSymbol = '♥';
+            break;
+        case 'clubs':
+            suitSymbol = '♣';
+            break;
+        case 'spades':
+            suitSymbol = '♠';
+            break;
+        default:
+            suitSymbol = '';
+    }
+    return (
+        <span className={`card rank-${rank.toLowerCase()} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{suitSymbol}</span>
+        </span>
+    );
+};
+
+export default CardElement;
